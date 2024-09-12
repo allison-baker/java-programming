a2b4ba0cadba268838c63b8b6901ddbdca1295aa
allison-baker <baker.allison.m@gmail.com>
2024-09-11 18:30:38 -0600

    finish assignment 02 tests

--- assignment-02/src/test/java/LargestElementFinderTest.java
+++ assignment-02/src/test/java/LargestElementFinderTest.java
@@ -20,4 +20,37 @@ public class LargestElementFinderTest {
         int[] expected = {1, 2};
         assertArrayEquals(expected, LargestElementFinder.locateLargest(array));
     }
+
+    @Test
+    void testLocateLargestFirst() {
+        double[][] array = {
+            {100, 35, 6, 95},
+            {40.5, 72, 32.4, 1.5},
+            {60, 4.7, 9.5, 11}
+        };
+        int[] expected = {0, 0};
+        assertArrayEquals(expected, LargestElementFinder.locateLargest(array));
+    }
+
+    @Test
+    void testLocateLargestLast() {
+        double[][] array = {
+                {2, 4.5, 15, 60},
+                {13.6, 5, 77, 23.5},
+                {30, 53, 8.3, 80}
+        };
+        int[] expected = {2, 3};
+        assertArrayEquals(expected, LargestElementFinder.locateLargest(array));
+    }
+
+    @Test
+    void testLocateLargestSame() {
+        double[][] array = {
+                {5, 5, 5, 5},
+                {5, 5, 5, 5},
+                {5, 5, 5, 5}
+        };
+        int[] expected = {0, 0};
+        assertArrayEquals(expected, LargestElementFinder.locateLargest(array));
+    }
 }
