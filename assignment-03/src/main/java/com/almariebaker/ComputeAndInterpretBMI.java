8ff77371105010d91b8ba0c85515cea6e9ec040a
allison-baker <baker.allison.m@gmail.com>
2024-09-15 21:15:53 -0600

    finish assignment 3

--- assignment-03/src/main/java/com/almariebaker/ComputeAndInterpretBMI.java
+++ assignment-03/src/main/java/com/almariebaker/ComputeAndInterpretBMI.java
@@ -1,6 +1,6 @@
 // Project Prolog
 // Name: Al Baker
-// CS 3250 Section 601
+// CS3250 Section 601
 // Project: Assignment 03
 // Date: 09/14/2024
 // Purpose: This program takes the weight and height from the user and outputs the BMI.
@@ -11,26 +11,35 @@ import java.util.Scanner;
 
 public class ComputeAndInterpretBMI {
     public static void main(String[] args) {
+        // Prepare to get user input from terminal
         Scanner input = new Scanner(System.in);
 
         // Prompt the user to enter weight in pounds
         System.out.print("Enter weight in pounds: ");
         double weight = input.nextDouble();
 
-        // Prompt the user to enter height in inches
+        // Prompt the user to enter height with one value each for feet and inches
         System.out.print("Enter height in two values - feet and inches: ");
+
+        // Calculate the height in just inches
         double height = calculateHeight(input.nextDouble(), input.nextDouble());
 
-        // Display result
+        // Calculate the BMI with the weight and height
         double bmi = calculateBMI(weight, height);
+
+        // Print the BMI and interpretation to terminal
         System.out.println("BMI is " + Math.round(bmi * 100.0) / 100.0);
         System.out.println(interpretBMI(bmi));
     }
 
+    /* ------------ SPLIT THE MAIN METHOD INTO THREE HELPER METHODS FOR EASIER TESTING ------------ */
+
+    // Takes two doubles for feet and inches and returns the height in inches
     public static double calculateHeight(double f, double i) {
         return (f * 12) + i;
     }
 
+    // Takes two doubles for weight and height and returns the BMI
     public static double calculateBMI(double w, double h) {
         final double KILOGRAMS_PER_POUND = 0.45359237; // Constant
         final double METERS_PER_INCH = 0.0254; // Constant
@@ -43,6 +52,7 @@ public class ComputeAndInterpretBMI {
         return weightInKilograms / (heightInMeters * heightInMeters);
     }
 
+    // Takes a double for BMI and returns a string interpretation
     public static String interpretBMI(double bmi) {
         if (bmi < 18.5)
             return "Underweight";
--- assignment-03/src/test/java/ComputeAndInterpretBMITest.java
+++ assignment-03/src/test/java/ComputeAndInterpretBMITest.java
@@ -1,6 +1,6 @@
 // Project Prolog
 // Name: Al Baker
-// CS 3250 Section 601
+// CS3250 Section 601
 // Project: Assignment 03
 // Date: 09/14/2024
 // Purpose: To test the ComputeAndInterpretBMI class.
@@ -10,8 +10,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.almariebaker.ComputeAndInterpretBMI;
 
 public class ComputeAndInterpretBMITest {
+    // Test the calculateHeight method, including using heights with decimals in the inches values
     @Test
-    public void testComputeAndInterpretBMI() {
+    public void testCalculateHeight() {
+        assertEquals(60, ComputeAndInterpretBMI.calculateHeight(5, 0));
+        assertEquals(75.5, ComputeAndInterpretBMI.calculateHeight(6, 3.5));
+        assertEquals(70, ComputeAndInterpretBMI.calculateHeight(5, 10));
+        assertEquals(72.5, ComputeAndInterpretBMI.calculateHeight(6, 0.5));
+    }
 
+    // Test the calculateBMI method, using some weights with decimals and the heights from the last test
+    @Test
+    public void testCalculateBMI() {
+        assertEquals(30.37, ComputeAndInterpretBMI.calculateBMI(155.5, 60), 0.01);
+        assertEquals(22.20, ComputeAndInterpretBMI.calculateBMI(180, 75.5), 0.01);
+        assertEquals(18.15, ComputeAndInterpretBMI.calculateBMI(126.5, 70), 0.01);
+        assertEquals(27.69, ComputeAndInterpretBMI.calculateBMI(207, 72.5), 0.01);
+    }
+
+    // Test the interpretBMI method, using the BMIs from the last test
+    @Test
+    public void testInterpretBMI() {
+        assertEquals("Obese", ComputeAndInterpretBMI.interpretBMI(30.37));
+        assertEquals("Normal", ComputeAndInterpretBMI.interpretBMI(22.20));
+        assertEquals("Underweight", ComputeAndInterpretBMI.interpretBMI(18.15));
+        assertEquals("Overweight", ComputeAndInterpretBMI.interpretBMI(27.69));
     }
 }
