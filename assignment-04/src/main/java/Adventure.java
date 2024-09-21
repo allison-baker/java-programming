4f882a8371ecd3ccc34f0069adf66cab08f3af6e
allison-baker <baker.allison.m@gmail.com>
2024-09-20 22:30:12 -0600

    finish assignment 4

--- assignment-04/src/main/java/Adventure.java
+++ assignment-04/src/main/java/Adventure.java
@@ -8,7 +8,7 @@
 /* ---------- COMMANDS ---------- */
 // go <direction> - options are north, south, east, or west
 // inventory - list the items in the inventory
-// brass lantern, rope, rations, staff
+    // brass lantern, rope, rations, staff
 // quit - exit the program
 
 /* ---------- SPECIFICATIONS ---------- */
@@ -22,24 +22,140 @@
 // Print an error message if the player tries to move out of bounds
 // Must have unit tests that cover all functionality
 
-/* ---------- TIPS ---------- */
-// Get input with java.util.Scanner
-// Break input line into words with String.split()
-// Chapters 2, 3, 6, and 9 have information on compiling/running programs, Scanner class,
-// working with strings and arrays, and creating/using classes/objects
-
 import java.util.*;
 
 public class Adventure {
+    int currRow;
+    int currCol;
+    char currDirection = Character.MIN_VALUE;
+    String currCommand = null;
+    static int MAX_ROWS = 5;
+    static int MAX_COLS = 5;
+    static int MIN_ROWS = 0;
+    static int MIN_COLS = 0;
+    static String[] INVENTORY = new String[4];
+
+    public Adventure(int row, int col, String[] inventory) {
+        currRow = row;
+        currCol = col;
+        INVENTORY = inventory;
+    }
+
+    void printLocation() {
+        System.out.printf("Your current location is: {%1$d, %2$d}%n", currRow, currCol);
+        System.out.print("Enter a command: ");
+    }
+
+    void processGo() {
+        // Switch case for processing direction
+        switch(currDirection) {
+            case 'n':
+            case 'N':
+                if (currCol > MIN_COLS) {
+                    currCol--;
+                } else {
+                    System.out.println("You cannot go north from here. Try another direction.");
+                }
+                break;
+            case 'e':
+            case 'E':
+                if (currRow < MAX_ROWS - 1) {
+                    currRow++;
+                } else {
+                    System.out.println("You cannot go east from here. Try another direction.");
+                }
+                break;
+            case 's':
+            case 'S':
+                if (currCol < MAX_COLS - 1) {
+                    currCol++;
+                } else {
+                    System.out.println("You cannot go south from here. Try another direction.");
+                }
+                break;
+            case 'w':
+            case 'W':
+                if (currRow > MIN_ROWS) {
+                    currRow--;
+                } else {
+                    System.out.println("You cannot go west from here. Try another direction.");
+                }
+                break;
+            default:
+                System.out.println("Invalid direction. Valid directions are: 'north', 'east', 'south', or 'west'.");
+                break;
+        }
+    }
+
+    void printInventory() {
+        System.out.println("Your current inventory:");
+        for (String item : INVENTORY) {
+            System.out.println(item);
+        }
+    }
+
+    void processQuit() {
+        System.out.printf("Your current location is: {%1$d, %2$d}%n", currRow, currCol);
+        throw new RuntimeException("Farewell!");
+    }
+
+    public void startAdventure(String command) {
+        // While there is input, process commands
+        // Commands can be abbreviated, commands are not case-sensitive, and location is printed after each command
+        switch(command.charAt(0)) {
+            // All four go parameters work, location is correct, location stays in bounds, error handling
+            case 'g':
+            case 'G':
+                currCommand = "go";
+                // Get second word from line for direction
+                String[] commands = command.split(" ");
+                if (commands.length > 1) {
+                    currDirection = commands[1].charAt(0);
+                    processGo();
+                } else {
+                    System.out.println("Must include a direction with 'go' command.");
+                }
+                printLocation();
+                break;
+
+            // Inventory command prints inventory
+            case 'i':
+            case 'I':
+                currCommand = "inventory";
+                printInventory();
+                printLocation();
+                break;
+
+            // Quit command exits the program
+            case 'q':
+            case 'Q':
+                currCommand = "quit";
+                processQuit();
+                break;
+
+            // Prints error messages for invalid commands
+            default:
+                System.out.println("Invalid command. Valid commands are: 'go <direction>', 'inventory', or 'quit'.");
+                printLocation();
+                break;
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: Inventory command prints inventory
-        // TODO: commands can be abbreviated
-        // TODO: Commands are not case sensitive
-        // TODO: Location is printed after each command
-        // TODO: Location is correct
-        // TODO: Location is not out of bounds, and the program prints a message if a go command would take the player out of bounds
-        // TODO: All four go parameters work
-        // TODO: Prints error messages for invalid commands and parameters
-        // TODO: Quit command exits the program
+        Adventure adventure = new Adventure(0, 0, new String[]{"brass lantern", "rope", "rations", "staff"});
+
+        // Prepare to get user input from terminal
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a command: ");
+
+        while(input.hasNext()) {
+            try {
+                String command = input.nextLine();
+                adventure.startAdventure(command);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
     }
 }
--- assignment-04/src/test/java/AdventureTest.java
+++ assignment-04/src/test/java/AdventureTest.java
@@ -6,14 +6,122 @@
 // Purpose: This program tests the Adventure class.
 
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AdventureTest {
-    // TODO: Test inventory command
-    // TODO: Test abbreviated commands
-    // TODO: Test case-insensitive commands
-    // TODO: Test boundaries
-    // TODO: Test all four go directions
-    // TODO: Test invalid commands
-    // TODO: Test quit command
+    private final Adventure adventureTest = new Adventure(0, 0, new String[] {"brass lantern", "rope", "rations", "staff"});
+
+    // Test print inventory method
+    @Test
+    public void testPrintInventory() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        // Call print inventory method
+        adventureTest.printInventory();
+
+        // Check output
+        String output = outputStream.toString();
+        String expected = String.format("Your current inventory:%1$sbrass lantern%1$srope%1$srations%1$sstaff", System.lineSeparator());
+        assertEquals(expected, output.trim());
+
+        System.setOut(originalOut);
+    }
+
+    // Test abbreviated commands
+    @Test
+    public void testAbbreviations() {
+        assertAll("abbreviations",
+            () -> {
+                adventureTest.startAdventure("g n ");
+                assertEquals("go", adventureTest.currCommand);
+            },
+            () -> {
+                adventureTest.startAdventure("i");
+                assertEquals("inventory", adventureTest.currCommand);
+            },
+            () -> {
+                Exception e = assertThrows(RuntimeException.class, () -> adventureTest.startAdventure("q"));
+                assertEquals("quit", adventureTest.currCommand);
+            }
+        );
+    }
+
+    // Test case-insensitive commands
+    @Test
+    public void testCases() {
+        assertAll("uppercase",
+                () -> {
+                    adventureTest.startAdventure("G N");
+                    assertEquals("go", adventureTest.currCommand);
+                },
+                () -> {
+                    adventureTest.startAdventure("I");
+                    assertEquals("inventory", adventureTest.currCommand);
+                },
+                () -> {
+                    Exception e = assertThrows(RuntimeException.class, () -> adventureTest.startAdventure("Q"));
+                    assertEquals("quit", adventureTest.currCommand);
+                }
+        );
+    }
+
+    // Test boundaries
+    @Test
+    public void testBoundaries() {
+        assertAll("boundaries",
+            () -> {
+                adventureTest.startAdventure("g n");
+                assertEquals(0, adventureTest.currCol);
+            },
+            () -> {
+                for (int i =0; i<5; i++) {
+                    adventureTest.startAdventure("g s");
+                }
+                assertEquals(4, adventureTest.currCol);
+            },
+            () -> {
+                adventureTest.startAdventure("g w");
+                assertEquals(0, adventureTest.currRow);
+            },
+            () -> {
+                for (int i =0; i<5; i++) {
+                    adventureTest.startAdventure("g e");
+                }
+                assertEquals(4, adventureTest.currRow);
+            });
+    }
+
+    // Test invalid commands
+    @Test
+    public void testInvalidCommands() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        adventureTest.startAdventure("x");
+        assertEquals("Invalid command. Valid commands are: 'go <direction>', 'inventory', or 'quit'.", outputStream.toString().split(System.lineSeparator())[0]);
+
+        System.setOut(originalOut);
+    }
+
+    // Test go command with no direction
+    @Test
+    public void testNoDirection() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        adventureTest.startAdventure("g");
+        assertEquals("Must include a direction with 'go' command.", outputStream.toString().split(System.lineSeparator())[0]);
+
+        System.setOut(originalOut);
+    }
 }
